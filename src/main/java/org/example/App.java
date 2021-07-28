722dc758b03d96923b4b34866a1bcd0bcf76c8b9
qixuan <1124035186@qq.com>
2021-07-28 12:31:02 +0800

    No2

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,8 +6,7 @@ package org.example;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
         Teacher teacher = new Teacher(1,"小刚","男",15);
         Student student = new Student(1,"小红","女",30);
