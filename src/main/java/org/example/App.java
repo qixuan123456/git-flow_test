a7647c29e15c2dd5a0ada73115ad14a785c18ca6
qixuan <1124035186@qq.com>
2021-07-28 12:16:49 +0800

    add main

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,5 +9,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Teacher teacher = new Teacher(1,"小刚","男",15);
+        Student student = new Student(1,"小红","女",30);
+        teacher.teach();
+        student.study();
     }
 }
